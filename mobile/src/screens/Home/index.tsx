c2695f564f68586af64e69cc5cf4252cfff58bab
GustBitencourt <gubiten@gmail.com>
2022-09-14 15:41:40 -0300

    MOBILE02 - Component GAMECARD estrutura e estilo

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,13 @@
 import { View, Image } from 'react-native';
 
+import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+
 import { styles } from './styles';
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { Heading } from '../../components/Heading';
+
+import { GAMES } from '../../utils/games';
+
 
 export function Home() {
   return (
@@ -15,6 +20,10 @@ export function Home() {
       <Heading 
         title="Encontre o seu duo"
         subtitle="Selecione o jogo que quer jogar..." 
+      />
+
+      <GameCard 
+        data={GAMES[0]}
       />
     </View>
   );
