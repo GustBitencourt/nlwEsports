4ac72b4b685268358ba7b8ab83698780dabf34f2
GustBitencourt <gubiten@gmail.com>
2022-09-15 13:55:31 -0300

    BACKEND03 - rascunho de rotas, configuracao de prisma para o sqlite como banco de dados e criacao de modelo de Game

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,19 @@ import express from "express";
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([]);
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    const gameId = request.params.id;
+
+    return response.send(gameId)
+
     return response.json([
         {
             id: 1,
@@ -17,6 +29,14 @@ app.get('/ads', (request, response) => {
             name: 'Ads 3',
         },
     ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    const adsId = request.params.id;
+    
+    return response.send(adsId);
+
+    return response.json();
 })
 
 app.listen(3333);
